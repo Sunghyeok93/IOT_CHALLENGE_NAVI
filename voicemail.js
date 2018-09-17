c1ff7a44714bf94de2b6e0cd63d5cbd79f6a3410
Ahn <ahnjunghyun450@gmail.com>
2018-09-17 12:40:55 +0000

    voicemail 수정 완료

--- voicemail.js
+++ voicemail.js
@@ -30,23 +30,23 @@ async function ttsCommand(msg) {
 module.exports = {
     sendMailModule : async function(){
         await ttsCommand("보내실 메시지를 말씀하세요.");
-        mail = await sttCommand('2');
+        mail = "테스트 중입니다";
+     //   mail = await sttCommand('2');
         console.log(mail);
-	const message = [ { "content":mail, "sender":"ARTIK" } ];        
-
-
+	const message = [{ "content":mail, "sender":"ARTIK" }];     
         const MAIL_URL = 'http://ec2-54-180-8-155.ap-northeast-2.compute.amazonaws.com:5000/voicemail';
         const mailUrl = new URL(MAIL_URL);
         const mailOptions = {
-        url: mailUrl.toString(),
+          url: mailUrl.toString(),
           method: 'POST',
-          headers: {'Authorization': 'ABC'},
-	  body:{body:JSON.stringify({message})}
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+          form: {'content' : mail, 'sender':'ARTIK'}
+	 // body:{body:JSON.stringify(message)}
         };
         const mailResult = await request(mailOptions);
         console.log(mailResult);
         
-        if(mailResult === 200){
+        if(mailResult == 200){
             await ttsCommand("메시지가 정상 전달 되었습니다.");
         }
         else{
